8f1da031ce82430215eeb16331408e20c73fcb61
aniketTripathi687 <a.tripathi@students.htl-leonding.ac.at>
2026-02-11 12:23:54 +0100

    added angular and html page

---
+++ frontend/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgIf} from "@angular/common";
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
+    styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    //TODO Handle login logic here
+    const credentials = {
+      username: this.f['username'].value,
+      password: this.f['password'].value
+    };
+
+    console.log('Login credentials:', credentials);
+    // TODO Implement auth. services
+  }
+
+  resetForm() {
+    this.submitted = false;
+    this.loginForm.reset();
+  }
+}
