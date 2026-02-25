86725c3e2d272544aec1f7570fd4303621c0dc6a
Alexfly142 <o.mukhin@students.htl-leonding.ac.at>
2026-02-25 20:32:12 +0100

    created functional login (BETA)

--- src/frontend/login/login.component.ts
+++ src/frontend/login/login.component.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {NgOptimizedImage} from '@angular/common';
-import {RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink, Router } from '@angular/router';
+import { AuthService } from '../../app/services/auth.service';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
+  selector: 'app-login',
+  templateUrl: './login.component.html',
   imports: [
     ReactiveFormsModule,
     NgOptimizedImage,
     RouterLink
   ],
-    styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   loginForm: FormGroup;
   submitted = false;
+  protected errorMessage = '';
+  protected loading = false;
 
-  constructor(protected formBuilder: FormBuilder) {
+  constructor(
+    protected formBuilder: FormBuilder,
+    protected authService: AuthService,
+    protected router: Router
+  ) {
     this.loginForm = this.formBuilder.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
       password: ['', [Validators.required, Validators.minLength(6)]]
@@ -30,23 +37,39 @@ export class LoginComponent {
 
   onSubmit() {
     this.submitted = true;
+    this.errorMessage = '';
 
     if (this.loginForm.invalid) {
       return;
     }
 
-    //TODO Handle login logic here
+    this.loading = true;
+
     const credentials = {
       username: this.f['username'].value,
       password: this.f['password'].value
     };
 
-    // console.log('Login credentials:', credentials);
-    // TODO Implement auth. services
+    this.authService.login(credentials).subscribe({
+      next: (response) => {
+        this.loading = false;
+        if (response.success) {
+          // Redirect to menu on successful login
+          this.router.navigate(['/menu']);
+        } else {
+          this.errorMessage = response.error || 'Login failed';
+        }
+      },
+      error: (error) => {
+        this.loading = false;
+        this.errorMessage = error.message || 'An error occurred during login';
+      }
+    });
   }
 
   resetForm() {
     this.submitted = false;
+    this.errorMessage = '';
     this.loginForm.reset();
   }
 }
