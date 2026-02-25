515c03651e62a18d71d2c34e9331ef42a8a4e2f6
Alexfly142 <o.mukhin@students.htl-leonding.ac.at>
2026-02-25 20:32:42 +0100

    created functional register (NVM IT"S NOT BETA IT'S MEGA PRE ALPHA)

--- src/frontend/register/register.ts
+++ src/frontend/register/register.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
-import {LoginComponent} from '../login/login.component';
-import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { NgOptimizedImage } from '@angular/common';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+import { AuthService } from '../../app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -14,23 +15,47 @@ import { RouterLink } from '@angular/router';
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register extends LoginComponent{
-  constructor(formBuilder: FormBuilder) {
-    super(formBuilder);
+export class Register extends LoginComponent {
+  override errorMessage = '';
+  override loading = false;
+
+  constructor(
+    formBuilder: FormBuilder,
+    authService: AuthService,
+    router: Router
+  ) {
+    super(formBuilder, authService, router);
   }
 
-  onRegister(){
+  onRegister() {
     this.submitted = true;
+    this.errorMessage = '';
 
-    if(this.loginForm.invalid){
+    if (this.loginForm.invalid) {
       return;
     }
 
-    // TODO handle database insert here
+    this.loading = true;
+
     const credentials = {
       username: this.f['username'].value,
       password: this.f['password'].value
     };
 
+    this.authService.register(credentials).subscribe({
+      next: (response) => {
+        this.loading = false;
+        if (response.success) {
+          // Redirect to menu on successful registration
+          this.router.navigate(['/menu']);
+        } else {
+          this.errorMessage = response.error || 'Registration failed';
+        }
+      },
+      error: (error) => {
+        this.loading = false;
+        this.errorMessage = error.message || 'An error occurred during registration';
+      }
+    });
   }
 }
