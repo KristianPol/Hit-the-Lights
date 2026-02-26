41b12c6a32b3d070ba92379bbef7a6bad007d175
KristianPolimac <150543678+KristianPol@users.noreply.github.com>
2026-02-26 11:48:38 +0100

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../frontend/login/login.component';
 import { Register } from '../frontend/register/register';
 import {MenuComponent} from '../frontend/menu/menu';
+import {ProfileComponent} from '../frontend/profile/profile';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: Register},
-    {path: 'menu' , component : MenuComponent}
+    {path: 'menu' , component : MenuComponent},
+    {path: 'profile', component : ProfileComponent}
+
 ];
