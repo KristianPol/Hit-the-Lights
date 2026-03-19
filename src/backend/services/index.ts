b4258b03ef2fb0f02664ea5f184a284954622f0c
KristianPolimac <150543678+KristianPol@users.noreply.github.com>
2026-03-19 10:41:54 +0100

    Imports so the program works

--- src/backend/services/index.ts
+++ src/backend/services/index.ts
@@ -2,3 +2,5 @@ export { RegistrationService } from './RegistrationService';
 export type { RegistrationRequest, RegistrationResponse } from './RegistrationService';
 export { AuthenticationService } from './AuthenticationService';
 export type { LoginRequest, LoginResponse } from './AuthenticationService';
+export { SongService } from './SongService';
+export type { AddSongRequest, AddSongResponse } from './SongService';
