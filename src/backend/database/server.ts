b18707e55d2154e178a46b797e651993455f0fb7
KristianPolimac <150543678+KristianPol@users.noreply.github.com>
2026-05-09 18:35:27 +0200

    Trying to fix issues with deployment

--- src/backend/database/server.ts
+++ src/backend/database/server.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { authRouter, songRouter, friendshipRouter, messageRouter } from "../routers";
 
 const app = express();
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = Number(process.env['PORT']) || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
