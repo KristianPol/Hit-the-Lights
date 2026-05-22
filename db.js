0e059ab1ca4bd1511b6f2d61081ba241f0440f1a
KristianPolimac <150543678+KristianPol@users.noreply.github.com>
2026-05-22 08:28:55 +0200

    Added db.js for the postgres db

---
+++ db.js
@@ -0,0 +1,8 @@
+﻿const postgres = require('postgres');
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:b80GkW0fsAtVCkaT@db.tubpwysojkbkzqsuwebs.supabase.co:5432/postgres';
+
+const sql = postgres(connectionString);
+
+module.exports = sql;
+
